b8194917c33cfa0ad4e9bb2defbee77bac36c35d
AlanRoy25 <alanroythomas4@gmail.com>
2024-08-06 18:21:07 +0530

    using hooks

---
+++ digitalclock/src/DigitalClock.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { useState } from "react";
+
+function DigitalClock(){
+
+  return(
+    <>
+    <div className="clock-container">
+      <div className="clock">
+        <span> 
+          00:00:00
+        </span>
+        </div> </div>
+    </>
+  )
+}
+
+export default DigitalClock;
